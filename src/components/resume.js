628fcfeb43a277908bdaf6b4dd357af4eec14b41
unknown <dbriksza@gmail.com>
2021-03-11 20:46:50 -0500

    final

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,20 +1,14 @@
 import React, { useEffect } from "react";
 
 const Resume = () => {
-  //   let Fuck = null;
-  // useEffect(() => {
-  //   document.getElementById("yert").style.height = document.getElementById(
-  //     "yert"
-  //   ).contentDocument.body.scrollHeight;
-  // }, []);
   return (
     <iframe
       className="iframe-loading"
-      src="https://drive.google.com/file/d/1ASQNVTrj6ZoyUeymN7T5achqd95fdNSt/preview"
+      src="https://drive.google.com/file/d/1epI30PIiO56RBXux1l58MV6ge8gONL2n/preview"
       width="640"
       height="480"
     ></iframe>
   );
 };
 export default Resume;
-// https://drive.google.com/file/d/1ASQNVTrj6ZoyUeymN7T5achqd95fdNSt/view?usp=sharing
+// https://drive.google.com/file/d/1epI30PIiO56RBXux1l58MV6ge8gONL2n/view?usp=sharing
